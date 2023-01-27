ac24e0b62980424443a8cfb91225e86d346ee687
Leandro Marin <marinlopezleandro@gmail.com>
2023-01-27 11:59:43 -0300

    Finishing Project 1

---
+++ 1-expanding-cards/script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+
+container.addEventListener('click', (event) => {
+  if (event.target.className === 'panel') {
+    Array.from(container.children).forEach(child => {
+      child.classList.remove('active');
+    })
+    event.target.classList.add('active')
+  }
+})
